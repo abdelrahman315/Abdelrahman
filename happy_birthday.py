93d51bc4069c8ddafe20f46fd13a0ba4065b35f2
abdelrahman315 <amg1945@g.rit.edu>
2024-09-04 14:59:16 +0400

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,10 @@
+def bday_message():
+    name= input("enter your name")
+    birthmonth= input("enter your birthmonth")
+    birthdaymonth= input("enter your birthday month")
+    birthyear= input("enter your birthyear")
+    print(name,",your birthday is on", birthmonth, birthdaymonth, birthyear,"!")
+bday_message()
+bday_message()
+bday_message()
+
